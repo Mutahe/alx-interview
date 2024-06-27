26ec00d19e9a1acf6bd610784dd5cefb0b787e54
Lizbeth <elizagatura@gmail.com>
2024-06-28 01:06:07 +0300

    utf

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def get_leading_set_bits(num):
+    """Returns the number of leading set bits (1)."""
+    set_bits = 0
+    helper = 1 << 7
+    while helper & num:
+        set_bits += 1
+        helper = helper >> 1
+    return set_bits
+
+def validUTF8(data):
+    """Determines if a given data set represents a valid UTF-8 encoding."""
+    bits_count = 0
+    
+    for byte in data:
+        if bits_count == 0:
+            bits_count = get_leading_set_bits(byte)
+            if bits_count == 0:
+                continue
+            if bits_count == 1 or bits_count > 4:
+                return False
+        else:
+            if not (byte & (1 << 7) and not (byte & (1 << 6))):
+                return False
+        bits_count -= 1
+
+    return bits_count == 0
